c2e67558a35e52cc402049438f320e6db1e01365
Viswanathan-04 <vish2004k@gmail.com>
2024-05-20 23:41:43 +0530

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -49,7 +49,7 @@ function Contact()
     const [message, setMessage] = useState("");
     return (
         <div className="main-5">
-            <h2 className="main-5-1-title" style={{textAlign: "center"}}>Connect to Me</h2>
+            <h2 className="main-5-1-title" style={{textAlign: "center"}}>Any Queries ?</h2>
             {val!=="Error Connecting to Network" ? <div className="main-5-1">
                 <form className="form-contact">
                     <input type="email" id="input1" placeholder="Mail-id" onChange={(event) => setMail(event.target.value)} required></input>
